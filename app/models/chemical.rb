faa45638d8c0b4103f8c2f9de5ca252e5bca958b
Chase Southard <chase.southard@gmail.com>
2009-01-10 12:45:34 -0500

    updated model for thinking sphinx; updated environment for gems

--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -1,2 +1,17 @@
 class Chemical < ActiveRecord::Base
+
+  #thinking_sphinx 
+  define_index do
+    indexes name
+    indexes cas
+    indexes formula
+    indexes location
+    indexes storage
+    indexes notes
+    indexes smiles
+    indexes inchi
+    
+    has created_at
+  end
+
 end
